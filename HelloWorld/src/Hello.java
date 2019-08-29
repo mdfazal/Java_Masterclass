a749523db52c8b828681c5f271c1dbe0df000d77
mdfazal <fazalmd964@gmail.com>
2019-08-29 06:40:59 +0530

    hello world

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main(String[] args)
+    {
+       System.out.println("Hello World");
+       System.out.println("I am Fazal.");
+       int n=(5+10)+(2*10);
+       int m=12;
+       int l=n*2;
+       int total=n+m+l;
+       System.out.println(total);
+       int last=1000-total;
+       System.out.println(last);
+    }
+}
